f68f07ebd0bd8c89979281d14f46521caa0a6e51
OwJuan <Juan.defreitas20@gmail.com>
2021-03-16 17:14:48 -0300

    primeiro commit

---
+++ src/mocks/users.js
@@ -0,0 +1,15 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Juan',
+    },
+    {
+        id: 2,
+        name: 'Rose',
+    },
+    {
+        id: 3,
+        name: 'Heloisa',
+    },
+
+];
